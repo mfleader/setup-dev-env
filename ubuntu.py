6b361afc33e778de4993c522b60e08e1cc55e2f2
Matthew  F Leader <mfleader42@gmail.com>
2019-07-22 18:34:48 -0400

    Commented r_edge() function.

--- ubuntu.py
+++ ubuntu.py
@@ -8,8 +8,12 @@ pkgs = [
 
 def r_edge():
     ubuntu_v = 'disco-cran35/'
+
+    # add required key to gpg key server
     sbp.run(['apt-key', 'adv', '--keyserver', 'keyserver.ubuntu.com', '--recv-keys', 'E298A3A825C0D65DFD57CBB651716619E084DAB9'])
+    # add repo for this ubuntu version's R edge
     sbp.run(['add-apt-repository', 'deb', 'https://cloud.r-project.org/bin/ubuntu', ubuntu_v])
+    # install R development package
     sbp.run(['apt', 'install', '-y', 'r-base-dev'])
 
 
