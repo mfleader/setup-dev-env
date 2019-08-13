c3c9522c5ee66ba71f6a2f472dd6238b606a43a1
Matthew  F Leader <mfleader42@gmail.com>
2019-08-13 19:06:03 -0400

    Add script to install and setup pyenv to work with pipenv

--- pyenv.sh
+++ pyenv.sh
@@ -1,2 +1,27 @@
 # set .python-version file with the name of a venv
-# pyenv local <venv-name> 
+# pyenv local <venv-name>
+
+# requirements
+sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+xz-utils tk-dev libffi-dev liblzma-dev libssl1.0-dev
+
+# revoke elevated permission, so everything is created user permissions
+sudo -k
+
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
+
+# it's better for pipenv to discover pyenv python versions
+echo 'export PIPENV_PYTHON="$PYENV_ROOT/shims/python"' >> ~/.bashrc
+
+
+if [-f ~/.zshrc]; then
+  echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+  echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+  echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
+  # it's better for pipenv to discover pyenv python versions
+  echo 'export PIPENV_PYTHON="$PYENV_ROOT/shims/python"' >> ~/.zshrc
+fi
